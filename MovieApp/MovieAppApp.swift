61a271d2590cb56c8e46a057e126ca428eac620c
ntphuongHD <phuong.nguyen@hdwebsoft.dev>
2024-02-24 23:51:52 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by mac on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
